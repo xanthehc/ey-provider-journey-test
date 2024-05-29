d211161e0dd7e7f0f1994b74999d4803f47730bb
xanthehc <Xanthe.HUGHES-CAMPBELL@EDUCATION.GOV.UK>
2024-05-29 14:40:21 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,13 @@
 const { NotifyClient } = require("notifications-node-client");
 
-const apiKey = 'magic_link-0b94efef-1e60-4200-a2b8-ec7a2d545fd9-6d159ebf-6d1a-4350-a9c9-7a21fd21c03d'; // replace with your actual API key
+const apiKey = ''; // replace with your actual API key
 const templateId = 'e571db62-1c28-4d43-990b-ad856dd47bbf';
 const notifyClient = new NotifyClient(apiKey);
 
-function sendEmail(email) {
-  notifyClient.sendEmail(templateId, email)
+notifyClient.sendEmail(templateId, "xanthe.hughes-campbell@education.gov.uk")
   .then(response => {
     console.log("Email sent successfully:", response);
   })
   .catch(error => {
     console.error("Error sending email:", error);
   });
-}
-
-module.exports = sendEmail; // Export the sendEmail function
